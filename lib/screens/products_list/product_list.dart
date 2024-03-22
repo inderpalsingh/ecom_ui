048bce09552de83ea2efe685034cae50042b6c21
Inderpal Singh <inderpalsmutta@gmail.com>
2024-03-22 06:58:02 +0530

    Latest

--- lib/screens/products_list/product_list.dart
+++ lib/screens/products_list/product_list.dart
@@ -1,3 +1,4 @@
+import 'package:ecom_ui/services/api_service.dart';
 import 'package:flutter/material.dart';
 
 class ProductListScreen extends StatelessWidget {
@@ -9,10 +10,11 @@ class ProductListScreen extends StatelessWidget {
       child: ListView.builder(
           itemBuilder: (context,index){
             return Container(
-              
               decoration: BoxDecoration(
                 color: Colors.grey.shade300,
-                
+              ),
+              child: CircleAvatar(
+                backgroundImage: NetworkImage(ApiService.categoriesList[index]['image'].toString())
               ),
             );
           }),
