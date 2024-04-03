3c3887da2a76a2f679df0dc05d11accfbc43f289
Inderpal Singh <inderpalsmutta@gmail.com>
2024-04-03 21:30:14 +0530

    Latest

--- lib/screens/products_list/product_list.dart
+++ lib/screens/products_list/product_list.dart
@@ -3,9 +3,6 @@ import 'package:flutter/material.dart';
 
 class ProductListScreen extends StatelessWidget {
   const ProductListScreen({super.key});
-
-   
-  
   
   @override
   Widget build(BuildContext context) {
