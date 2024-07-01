10ea20e8ea070011048e396dbb52507e34c3df38
Inderpal Singh <inderpalsmutta@gmail.com>
2024-07-01 21:55:04 +0530

    bloc added for login

--- lib/presentation/screens/login/login_page.dart
+++ lib/presentation/screens/login/login_page.dart
@@ -12,65 +12,67 @@ class LoginPage extends StatelessWidget {
     TextEditingController passController = TextEditingController();
 
     return Scaffold(
-      appBar: AppBar(title: const Text('Login')),
-      body: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  TextField(
-                    controller: userController,
-                    decoration: InputDecoration(
-                        hintText: 'Email',
-                        prefixIcon: const Icon(Icons.email),
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10),
-                        )),
-                  ),
-                  const SizedBox(height: 20),
-                  TextField(
-                    controller: passController,
-                    decoration: InputDecoration(
-                        hintText: 'Password',
-                        prefixIcon: const Icon(Icons.lock_open_rounded),
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10),
-                        )),
-                  ),
-                  const SizedBox(height: 50),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                      ElevatedButton(
-                        style: ElevatedButton.styleFrom(backgroundColor: Colors.blue),
-                          onPressed: () {
-                            if(userController.text.isNotEmpty && passController.text.isNotEmpty){
-                              context.read<LoginBloc>().add(UserLoginEvent(username: userController.text, password: passController.text));
+        appBar: AppBar(title: const Text('Login')),
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextField(
 
-                              Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=> const SplashPage()));
-                            } else{
-                              ScaffoldMessenger.of(context).showSnackBar(
-                                const SnackBar(content: Text('Worng credentials !!'),
-                                behavior: SnackBarBehavior.floating,
-                                ),
-
-                              );
-                            }
-
-                          },
-                          child: const Text('Login', style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
-                        )),
-                      ElevatedButton(
-                          style: ElevatedButton.styleFrom(backgroundColor: Colors.blue),
-                          onPressed: () {
-                            // Navigator.pop(context);
-                          },
-                          child: const Text('Cancel', style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold))),
-                    ],
-                  )
-                ],
+                controller: userController,
+                decoration: InputDecoration(
+                    hintText: 'Email',
+                    prefixIcon: const Icon(Icons.email),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    )),
               ),
-            ));
-          }
+              const SizedBox(height: 20),
+              TextField(
+                controller: passController,
+                decoration: InputDecoration(
+                    hintText: 'Password',
+                    prefixIcon: const Icon(Icons.lock_open_rounded),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    )),
+              ),
+              const SizedBox(height: 50),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  ElevatedButton(
+                      style: ElevatedButton.styleFrom(backgroundColor: Colors.blue),
+                      onPressed: () {
+                        if (userController.text.isNotEmpty && passController.text.isNotEmpty) {
+                          context.read<LoginBloc>().add(UserLoginEvent(username: userController.text, password: passController.text));
 
-        }
+                          Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => const SplashPage()));
+                        }
+                        // else {
+                        //   ScaffoldMessenger.of(context).showSnackBar(
+                        //     const SnackBar(
+                        //       content: Text('Worng credentials !!'),
+                        //       behavior: SnackBarBehavior.floating,
+                        //     ),
+                        //   );
+                        // }
+                      },
+                      child: const Text(
+                        'Login',
+                        style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+                      )),
+                  ElevatedButton(
+                      style: ElevatedButton.styleFrom(backgroundColor: Colors.blue),
+                      onPressed: () {
+                        // Navigator.pop(context);
+                      },
+                      child: const Text('Cancel', style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold))),
+                ],
+              )
+            ],
+          ),
+        ));
+  }
+}
